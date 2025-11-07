6e0fe5721f2d443a72629d0d8b8cfcd470e15ba3
Alexandr <alexandrlachin989@gmail.com>
2025-11-08 00:32:31 +0300

    конфиги

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+function cssLoaders(extra = null) {
+    const loaders = [
+        (new MiniCssExtractPlugin()).loader,
+        {
+            loader: 'css-loader',
+            options: {
+                importLoaders: 1,
+            },
+        }
+    ];
+
+    if (extra) loaders.push(extra);
+
+    return loaders;
+}
+
+module.exports = {
+    entry: './test/main.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+
+            {
+          test: /\.s[ac]ss$/, use: [...cssLoaders({
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                quietDeps: true,// Отключает предупреждения о депрекации
+                implementation: require('sass'),
+              }
+            }
+          })]
+        },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: './bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        // library: 'EditorJS',
+        // libraryTarget: 'umd',
+        // globalObject: 'this'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './test/index.html'
+        })
+    ],
+    devServer: {
+        static: './web',
+        port: 3000,
+        open: true
+    },
+};
